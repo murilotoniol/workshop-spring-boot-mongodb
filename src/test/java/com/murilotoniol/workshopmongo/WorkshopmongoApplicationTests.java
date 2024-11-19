f8eb6a82ac0c25025ed9d272121ec40f8aa65fa0
Murilo Besson <murilo.besson@outlook.com>
2024-11-19 16:44:42 -0300

    Primeiro commit - Projeto Criado

---
+++ src/test/java/com/murilotoniol/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilotoniol.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
